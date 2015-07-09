6e9c62c016b10b5838cbcd9506c341c2ebfa340f
Diwaker Ghimire <idwaker@gmail.com>
2015-07-09 16:59:28 +0545

    add form and use docker compose from here

--- index.php
+++ index.php
@@ -4,14 +4,28 @@
 require 'vendor/autoload.php';
 
 
-$app = new \Slim\App;
+$settings = [
+    'DEBUG' => filter_var(getenv('DEBUG'), FILTER_VALIDATE_BOOLEAN),
+    'MODE' => getenv('MODE') !== false ? getenv('MODE') : 'production',
+];
+
+$app = new \Slim\App($settings);
+
 
 // settings
 
 
 // routes
 $app->get('/', function ($req, $resp) {
-    $resp->write("This is home page");
+    $html = "<html><head></head><body style='width: 640px;margin:0 auto;'>
+    <div><h3>Subscribe Newsletter</h3>
+    <form name='newsletter' action='/save' method='post'>
+    <label>Fullname: <input type='text' name='fullname' value=''/></label>
+    <label>Email: <input type='text' name='email' value=''/></label>
+    <input type='submit' value='Submit' name='submit' />
+    </form></div></body></html>";
+
+    $resp->write($html);
     return $resp;
 })->setName('home');
 
